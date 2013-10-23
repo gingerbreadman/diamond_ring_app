ce70a7d2c261d02938939099460a1bdea78efe45
Rahim Ladak <rahim.ladak@gmail.com>
2013-10-23 12:11:29 -0500

    Created New Rails App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DiamondRingApp::Application.config.secret_key_base = '0f1cac52b8afaf6dbfa3a1fa10b2ca952e6542003b55c8d8f7513091e12261695b2dd5df00ef8dbf440cb9143bb84ed094f38e27ad80e7700ec1e2643e22d010'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DiamondRingApp::Application.config.session_store :cookie_store, key: '_diamond_ring_app_session'
